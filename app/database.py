b497e0869c3f45ebe9e37d4bd7bf83f8bef16ed5
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-04-02 15:04:13 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, SQLModel, Session
 
 # DATABASE_URL = "postgresql+psycopg2://postgres:password@host.docker.internal:5432/kurvalf"
-DATABASE_URL = "postgresql+psycopg2://postgres:P@ssw0rd.99@http://103.171.85.26:5432/kurvalf"
+DATABASE_URL = "postgresql+psycopg2://postgres:password@103.171.85.26:5432/kurvalf"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
