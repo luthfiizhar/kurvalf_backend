9a0402f25839931fcb8139d78591333f92af01e3
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-04-19 22:40:53 +0700

    Update env.py

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from sqlmodel import SQLModel
-from models import  Product, Document, Certificate 
+from models import  Product, Document, Certificate, ReceivedEmail 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
