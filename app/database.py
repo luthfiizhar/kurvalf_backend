888d0601b9231e73605aefa3188b960605ff3b02
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-05-06 19:29:43 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,12 @@ from sqlmodel import create_engine, SQLModel, Session
 # DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/kurvalf"
 DATABASE_URL = "postgresql+psycopg2://postgres:password@103.171.85.26:5432/kurvalf"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True,connect_args={
+                  "keepalives": 1,
+                  "keepalives_idle": 30,
+                  "keepalives_interval": 10,
+                  "keepalives_count": 5,
+              },)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
